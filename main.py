e62421e8fa5ab627b3a9e8677612f35c00f0cc26
ice-bear <andydowsen47us@gmail.com>
2022-08-14 16:45:40 +0530

    Basic program commit

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import numpy as np
-
-# first dimensional array
-basic_array = np.array([1, 2, 3, 4, 5])
-print(basic_array)
-
-# tow dimensional array or three-dimensional array.
-three_dimensional_array = np.array([1, 2, 3, 4, 5], dtype=complex)
-print(three_dimensional_array)
-
-# numpy arrange;
-numpy_arrange = np.arange(10,50,100)
-print(numpy_arrange)
+print("basic code")
---
+++ python/basic.py
@@ -0,0 +1,21 @@
+string = "Basic string"
+print(string)
+
+# string formatting
+print("%s This is a string" % 'To the right')
+
+# lists
+fruit_names = ["Apple", "Banana", "Guava", "Papaya", "Mango"]
+print(fruit_names)
+print(fruit_names[0:3])
+
+# append list in fruit_names
+fruit_names.append("Papaya")
+print(fruit_names)
+
+# add an data in any list at your position
+fruit_names.insert(2, "Append function")
+print(fruit_names)
+
+# print length of any list like in my case this is the fruit_nams function
+print(min(fruit_names))
