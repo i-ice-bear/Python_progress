05a66ed6e9b5d9cf6697c28c3d1d5b172918431a
ice-bear <andydowsen47us@gmail.com>
2022-08-14 17:24:35 +0530

    File change commit

---
+++ python/file.py
@@ -0,0 +1,5 @@
+file1 = open("../generated/Jimmy.txt", "wb")
+print(file1.mode)
+print(file1.name)
+file1.write(bytes("Jimmy carson blah blah", "UTF-8"))
+file1.close()
---
+++ python/readfile.py
@@ -0,0 +1,3 @@
+file1 = open('../generated/Jimmy.txt')
+read_file = file1.read()
+print(read_file)
