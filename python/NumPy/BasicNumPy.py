51e4f482ff2a37ffb29b067109424726a8988f96
ice-bear <andydowsen47us@gmail.com>
2022-08-27 19:31:26 +0530

    working on numpy

---
+++ python/NumPy/BasicNumPy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+listArray = [1, 2, 3, 4]
+numArray = np.array(listArray)
+
+print(numArray)
+
+# arranging the numbers in numpy
+
+arrangeArray = np.arange(8)
+print(arrangeArray)
+
+floatArrange = np.arange(1, 2, 3, np.float32)
+print(floatArrange)
+
+# creating array with a numerical range using lines paceFunction()
+
+lineSpaceArray = np.linspace(1, 4, 6, 8)
+print(lineSpaceArray)
+
