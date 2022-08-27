1f9b078c41a0ee5176c94eecac518a4b5eb2a2ce
ice-bear <andydowsen47us@gmail.com>
2022-08-27 15:44:06 +0530

    Edited main.py file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("basic code")
+import numpy as np
+
+array_state = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+print(array_state)
+
+
+def argsFunction(*args):
+    print(type(args));
+    if len(args) == 3:
+        return "args len is : 3"
+    else:
+        print("args is initially working")
+
+
+argsFunction("Data")
