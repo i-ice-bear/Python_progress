5b5975c265adf33837720dba51247c626638ed72
ice-bear <andydowsen47us@gmail.com>
2022-08-28 12:14:06 +0530

    try-except and else with pyautogui example

--- python/intermediate/try-execpt-else.py
+++ python/intermediate/try-execpt-else.py
@@ -1,18 +1,34 @@
-def trailFunction(*args):
-    print(type(args))
+import pywhatkit
+import pywhatkit as whatsKit
+import requests
 
+fetchData = "https://newsapi.org/v2/everything?q=tesla&from=2022-07-23&sortBy=publishedAt&apiKey" \
+            "=cd6f44f57521402dab39db73fde51eb4 "
+consolidate_result = requests.get(fetchData)
+json_result = (consolidate_result.json())
+
+print(whatsKit)
+
+
+def sendMessage():
+    whatsKit.sendwhatmsg_instantly("+919990251251", "json_result")
+
+
+# try except and else functions
 
 try:
-    trailFunction("state")
+    sendMessage()
+    def argsFunction(*args):
+        print(type(args))
 
-except:
-    trailFunction("state-2")
-    print("working on except")
+    argsFunction("harry", "statements")
+
+except EOFError as EOFErrorStatement:
+    print(EOFErrorStatement)
 else:
-    trailFunction("state-3")
+    def sendAnother():
+        pywhatkit.sendwhatmsg_instantly("+919540262825","json_result")
+    sendAnother()
 
-    print("Working on else")
 finally:
-    trailFunction("state-4")
-
-    print("Working on finally")
+    print("program runs successfully")
