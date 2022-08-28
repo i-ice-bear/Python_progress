9ba388310d2b2f31bcd7d6e10034970d89669484
ice-bear <andydowsen47us@gmail.com>
2022-08-28 12:28:57 +0530

    New file commit

---
+++ python/Basic/newFIle.py
@@ -0,0 +1,5 @@
+file1 = open("../../generated/scratch.txt", "wb")
+file1.write(bytes("Alan walker, Patrick stump", "UTF-8"))
+print(file1)
+
+file1.close();
--- python/intermediate/try-execpt-else.py
+++ python/intermediate/try-execpt-else.py
@@ -18,8 +18,11 @@ def sendMessage():
 
 try:
     sendMessage()
+
+
     def argsFunction(*args):
         print(type(args))
+
 
     argsFunction("harry", "statements")
 
@@ -27,8 +30,25 @@ except EOFError as EOFErrorStatement:
     print(EOFErrorStatement)
 else:
     def sendAnother():
-        pywhatkit.sendwhatmsg_instantly("+919540262825","json_result")
+        pywhatkit.sendwhatmsg_instantly("+919540262825", "json_result")
+
+
     sendAnother()
 
 finally:
     print("program runs successfully")
+
+
+def kwargsItems(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(key, value)
+    if (len(kwargs == 3)):
+        print("Kwargs length is 3")
+        pywhatkit.sendwhatmsg_instantly("+919540262825", "kwargs length is 3")
+    else:
+        print("Kwargs length is didn't set initially")
+        pywhatkit.sendwhatmsg_instantly("+919540262825", "didn't set initially")
+
+
+kwargsItems()
