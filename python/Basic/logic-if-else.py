f5c7e2d5f10e5c8a1a779f07f6f6b8db135a90fc
ice-bear <andydowsen47us@gmail.com>
2022-10-20 15:28:16 +0530

    Short hand properties of if else

--- python/Basic/logic-if-else.py
+++ python/Basic/logic-if-else.py
@@ -9,3 +9,10 @@ elif(marks>50 and marks<60):
 else:
     print("Don't know what to do { Fail }");
 print("The grade is", marks);
+
+# shorthand like agar magar in single nagar
+a = int(input("Enter the first number : "));
+b = int(input("Enter the second number : "))
+
+if a<b: print("B is greater than a")
+else: print("A is smaller than b")
