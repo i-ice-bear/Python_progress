59554f0efba825511b5a0586b7cab484b51e5607
ice-bear <andydowsen47us@gmail.com>
2022-10-22 22:33:37 +0530

    fileIO basic statement in /intermediate/fileIo

---
+++ python/intermediate/fileIO.py
@@ -0,0 +1,23 @@
+"""
+    Use the file IO in python you can simply access the non-volatile memory,
+    there are two types of memory in the system.
+
+    1. Non-volatile-memory.
+    -> Volatile memory is a type of memory that maintains,
+       its data only while the device is powered.
+       If the power is interrupted for any reason, the data is lost.
+    2. Non-volatile-memory:
+    -> Non-volatile memory (NVM) or non-volatile storage is a type of computer memory
+       that can retain stored information even after power is removed.
+
+    # types of opening file
+    1. "r" : (read-mode) : Open the file in reading mode.
+    2. "w" : (write-mode) : Open the file in writing mode as well as reading mode.
+    3. "x" : (exclusive-creation) : Creates file if not exist.
+                                    If exist then it break or throw error.
+    4. "a" : (append) : Add more content to a file from end...
+    5. "t" : (text-mode) : Open file in text mode. to deal with string
+    6. "b" : (binary-mode) : Open the files in binary mode
+    7. "+" : (read-and-write) : Read and write of the file.
+
+    """
