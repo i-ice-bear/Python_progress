e069de6ed0d3e2594e0d98ebf0ee78f664502f3e
ice-bear <andydowsen47us@gmail.com>
2022-11-04 15:08:01 +0530

    Snake-water-gun game-logic commit

--- python/Projects/snake-water-gun.py
+++ python/Projects/snake-water-gun.py
@@ -16,11 +16,40 @@ print(random_attribute)
 
 
 def play_game():
+    chances_to_play = 2
     while True:
-        chances_to_play = 2
         player_win_stat = 0
         computer_win_stat = 0
         player_turn = input("Enter your attribute : ")
+
+        if random_attribute != player_turn:
+            chances_to_play -= 1
+            computer_win_stat += 1
+            print("You, are wrong")
+            print(f"Computer got {computer_win_stat} point")
+        elif random_attribute == player_turn:
+            chances_to_play -= 1
+            print(chances_to_play)
+            player_win_stat += 1
+            print(f"Aya men, You got {player_win_stat} point")
+        elif random_attribute == "snake" and random_attribute == "water":
+            chances_to_play -= 1
+            print("Continue")
+        elif random_attribute == "gun":
+            chances_to_play -= 1
+            print(f"Another misconception")
+        else:
+            print("Something misconception")
+        if chances_to_play == 0:
+            time.sleep(3)
+            print("Chances UP!")
+            if player_win_stat > computer_win_stat:
+                print(f"Computer wins with {computer_win_stat} points")
+            else:
+                print(f"\nPlayer wins")
+            break
+        else:
+            continue
 
 
 play_game()
