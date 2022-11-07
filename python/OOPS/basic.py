fc38a99b1b046c9bd45e25bd6a898ca749677464
ice-bear <andydowsen47us@gmail.com>
2022-11-07 15:02:45 +0530

    Append class basic concept in ./python/oops/basic.py

--- python/OOPS/basic.py
+++ python/OOPS/basic.py
@@ -1,14 +1,58 @@
-import os
+"""
+   :OOPS : Object oriented programming
+
+   :return: Class is an object-oriented programming concept where we create templates
+            of program to unfollow the DRY Concept. Where we build a program easily
+            and quickly with the help of class oops concept.
+
+   :param: DRY : Do not repeat yourself | concept.
+
+   :arg: A class is a blueprint or a template for creating objects
+         while an object is an instance or a copy of the class with
+         actual values.
+
+"""
 
 
-class Main:
-    def __init__(self, __date, __state, __package):
-        self.date = __date
-        self.state = __state
-        self.package = __package
+class __do__not_repeat:
+    """
+        Header class, This is an empty class where we create the
+        templates inside the class.  ` templates `
+        like def __init__():
+                 like class keywords.
+    """
+    pass
 
-    def outro(self):
-        print("Date is : " + self.date + " State library is : " + self.state + " Package name is : " + self.package)
 
-open = Main("40", "redux", "tensorflow")
-open.outro()
+def __instances_class():
+    """
+    :return: These keywords are the instances of the class, which
+             is defined on the uppers-ide of the program.
+             Where a variable is equal to the class .
+    """
+    variable_1 = __do__not_repeat()  # these instances are not using anywhere
+    _variable__2 = __do__not_repeat()
+
+
+def __argument_Class_append():
+    """
+    :return: These are the arguments of the variable which is defined and
+             equalised to the class __do_not_repeat, and we're adding the
+             value inside the class. Like we're adding the scripts in the template.
+
+    :parameter: We used the instances from the upside function to prevent the errors.
+                In the program for the compatibility purposes.
+    """
+    variable_1 = __do__not_repeat()
+    _variable__2 = __do__not_repeat()
+
+    variable_1.name = "Current-name"
+    variable_1.standard = "memory-locator"
+    variable_1.date_formation = "032"
+    print(variable_1.name)
+
+
+__argument_Class_append()
+
+if __name__ == "__main__":
+    print(__name__)
