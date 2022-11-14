58849e631900c5510cb7668d83b59319c7e4c5d8
ice-bear <andydowsen47us@gmail.com>
2022-11-14 19:35:43 +0530

    Appended Ascii banners in ./python./projects/utilities/banners/banner.py

---
+++ python/Projects/_library__class.py
@@ -0,0 +1,17 @@
+"""
+:return: Instructions of library class,
+:parameter: Requirements to make
+            1. Lend a book method
+                -- If a book is not present in the library then
+                   return a statement that book is not present
+                   in the library and also tells that, who owns
+                   the book.
+            2. Return a book method.
+            3. Add a book method
+            4. Display a book method
+:method-template: Library(list-of-books, library_name)
+:params: Run an infinite loop and get the information time to time
+
+"""
+
+
